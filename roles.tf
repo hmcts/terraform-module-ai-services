2839aa0827a0570c65822ed398e7f4f9457bfd9c
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-05 14:43:42 +0100

    add roles

--- roles.tf
+++ roles.tf
@@ -3,3 +3,33 @@ resource "azurerm_role_assignment" "blob_contributor_from_cognitive_account" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_cognitive_account.cognitive_account.identity[0].principal_id
 }
+
+resource "azurerm_role_assignment" "contributor_from_cognitive_account" {
+  scope                = var.storage_account_id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_cognitive_account.cognitive_account.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "blob_contributor_from_compute_instance" {
+  scope                = var.storage_account_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_machine_learning_compute_instance.compute_instance.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "contributor_from_compute_instance" {
+  scope                = var.storage_account_id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_machine_learning_compute_instance.compute_instance.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "blob_contributor_from_ml_workspace" {
+  scope                = var.storage_account_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_machine_learning_workspace.ml_workspace.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "contributor_from_ml_workspace" {
+  scope                = var.storage_account_id
+  role_definition_name = "Contributor"
+  principal_id         = azurerm_machine_learning_workspace.ml_workspace.identity[0].principal_id
+}
