2ebca71cb516cab5ef47b1517c8d2f22d73ffbbe
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-05 09:17:41 +0100

    add count index

--- roles.tf
+++ roles.tf
@@ -5,12 +5,14 @@ resource "azurerm_role_assignment" "blob_contributor_from_cognitive_account" {
 }
 
 resource "azurerm_role_assignment" "blob_contributor_from_compute_instance" {
+  count                = var.instances
   scope                = var.storage_account_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_machine_learning_compute_instance.compute_instance[count.index].identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "sa_contributor_from_compute_instance" {
+  count                = var.instances
   scope                = var.storage_account_id
   role_definition_name = "Contributor"
   principal_id         = azurerm_machine_learning_compute_instance.compute_instance[count.index].identity[0].principal_id
