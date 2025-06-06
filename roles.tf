c3b50d77d79cf01030028f0b79199e64ae9e756e
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-06-06 08:25:00 +0100

    use for_each

--- roles.tf
+++ roles.tf
@@ -11,15 +11,21 @@ resource "azurerm_role_assignment" "contributor_from_cognitive_account" {
 }
 
 resource "azurerm_role_assignment" "blob_contributor_from_compute_instance" {
+  for_each = tomap({
+    for i in azurerm_machine_learning_compute_instance.compute_instance : i.name => i.identity[0].principal_id
+  })
   scope                = var.storage_account_id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_machine_learning_compute_instance.compute_instance.identity[0].principal_id
+  principal_id         = each.value
 }
 
 resource "azurerm_role_assignment" "contributor_from_compute_instance" {
+  for_each = tomap({
+    for i in azurerm_machine_learning_compute_instance.compute_instance : i.name => i.identity[0].principal_id
+  })
   scope                = var.storage_account_id
   role_definition_name = "Contributor"
-  principal_id         = azurerm_machine_learning_compute_instance.compute_instance.identity[0].principal_id
+  principal_id         = each.value
 }
 
 resource "azurerm_role_assignment" "blob_contributor_from_ml_workspace" {
