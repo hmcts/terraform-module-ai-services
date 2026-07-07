cef2556b91b03f231939866679fc615c9419e0a7
Connor O'Kane <connor.o'kane@justice.gov.uk>
2026-07-07 12:36:11 +0100

    fixing example

--- example/provider.tf
+++ example/provider.tf
@@ -3,7 +3,8 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias           = "private_dns"
-  subscription_id = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348" # DTS-CFTPTL-INTSVC
+  alias                           = "private_dns"
+  subscription_id                 = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348" # DTS-CFTPTL-INTSVC
+  resource_provider_registrations = "none"
   features {}
 }
