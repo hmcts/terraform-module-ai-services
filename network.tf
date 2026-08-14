ccfa63fd58ed2aa4a9ce7d55a27b37fa4cf03f16
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-08-14 15:27:44 +0100

    fix: format

--- network.tf
+++ network.tf
@@ -25,7 +25,7 @@ resource "azurerm_private_endpoint" "foundry_private_endpoint" {
 }
 
 resource "azurerm_private_endpoint" "cognitive_private_endpoint" {
-  count = var.enable_managed_network && var.create_cognitive_account ? 1 : 0
+  count      = var.enable_managed_network && var.create_cognitive_account ? 1 : 0
   depends_on = [azurerm_cognitive_deployment.cognitive_deployment]
 
   name                = "${var.product}-cognitive-account-pe-${var.env}"
